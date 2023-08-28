aebd235f682640a4354bdeda946fde8fddea0e62
Lintang Kusuma <lintang@kusuma.local>
2023-08-28 22:57:29 +0700

    Part 6: Networking

---
+++ Part_3_Building_UI_With_Flutter/commons/api_path_constant.dart
@@ -0,0 +1,7 @@
+class ApiPathConstant {
+  static String topStories(String section) => '/topstories/v2/$section.json';
+
+  static const String mostPopular = '/mostpopular/v2/emailed/7.json';
+
+  static const String exploreArticles = '/search/v2/articlesearch.json';
+}
